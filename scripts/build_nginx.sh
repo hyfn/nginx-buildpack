7e2eadeaf9bdcaf532eeec3b61507600886acc15
Daniel Leavitt <daniel@hyfn.com>
2017-03-28 20:14:05 -0700

    ssl mk1

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,9 +9,9 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.11.3}
-PCRE_VERSION=${PCRE_VERSION-8.39}
-ZLIB_VERSION=${ZLIB_VERSION-1.2.8}
+NGINX_VERSION=${NGINX_VERSION-1.9.9}
+PCRE_VERSION=${PCRE_VERSION-8.37}
+HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.26}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.gz
@@ -42,6 +42,10 @@ echo "Downloading $zlib_url"
     --with-zlib=zlib-${ZLIB_VERSION} \
     --prefix=/tmp/nginx \
     --with-http_gzip_static_module \
+    --add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION} \
+    --with-http_v2_module \
+    --with-http_ssl_module \
+    --with-http_stub_status_module
     --with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' \
     --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed'
 
